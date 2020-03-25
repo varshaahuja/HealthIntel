b68af05d8e6b3ce0a8d60e9e8f404b1909252e1d
sp2728 <sp2728@njit.edu>
2020-03-25 15:17:55 -0400

    deploy

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/healthintel'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/healthintel/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
