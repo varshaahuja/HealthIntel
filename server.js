88cfa4c046db3950b8bdc749609d4010508c74cc
sp2728 <sp2728@njit.edu>
2020-03-25 15:26:05 -0400

    make it better

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/healthintel'));
+app.use(express.static(__dirname + '/dist/healthIntel'));
 
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/healthintel/index.html'));
+res.sendFile(path.join(__dirname+'/dist/healthIntel/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
