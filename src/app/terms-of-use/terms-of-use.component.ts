98f3804c0fbd8d9c250d8ecce6a51541345e26fe
sp2728 <sp2728@njit.edu>
2020-04-06 19:08:37 -0400

    fixed the bug for deployment

--- src/app/terms-of-use/terms-of-use.component.ts
+++ src/app/terms-of-use/terms-of-use.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms-of-use',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsOfUseComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private localStorageService:LocalStorageService, private router:Router) { }
+  user:any;
+  role:any;
   ngOnInit() {
+    this.user = this.localStorageService.getUser();
+    this.role = this.localStorageService.getRole();
+  }
+
+
+  logo(){
+
+    if(this.role == 'user'){
+      this.router.navigate(['/userDashboard']);
+    }
+    else if(this.role == 'admin'){
+      this.router.navigate(['/adminDashboard']);
+    }
+    else{
+      this.router.navigate(['/']);
+    }
   }
 
 }
